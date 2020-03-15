4e741f004b836cf815afd9845c9779fe8ae96f09
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-15 08:17:00 +0000

    Deploy dtmkeng/react-tailwind-css-example to github.com/dtmkeng/react-tailwind-css-example.git:gh-pages

---
+++ precache-manifest.25a9c4bf3290b0ac78b426709943dfd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "574b8c37bd27661590cc9ccd32ef4293",
+    "url": "/react-tailwind-css-example/index.html"
+  },
+  {
+    "revision": "c45f57f96257d409ef35",
+    "url": "/react-tailwind-css-example/static/css/main.07da3517.chunk.css"
+  },
+  {
+    "revision": "46ed6ee07facd4ff3b9e",
+    "url": "/react-tailwind-css-example/static/js/2.0c364b25.chunk.js"
+  },
+  {
+    "revision": "c45f57f96257d409ef35",
+    "url": "/react-tailwind-css-example/static/js/main.27eba58e.chunk.js"
+  },
+  {
+    "revision": "027f4bddb4cf5a736863",
+    "url": "/react-tailwind-css-example/static/js/runtime-main.e71a0f4e.js"
+  }
+]);
